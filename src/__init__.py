23a0918c9c0d72965c6f732ad842ff93742a538e
atsushiflee <atsushiflee@icloud.com>
2020-04-13 19:13:23 -0700

    fix cycle import

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,12 @@
-from views import *
-from models import *
+from . import views
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
+
+
 app.config.from_pyfile("../config.py")
 
 db = SQLAlchemy(app)
 db.init_app(app)
-
-
-if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True)
