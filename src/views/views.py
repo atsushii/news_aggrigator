723fe0da6b719e68cfe334d2e3352099ee1b166e
atsushiflee <atsushiflee@icloud.com>
2020-04-14 23:11:58 -0700

    Add if condition for checking empty sata

--- src/views/views.py
+++ src/views/views.py
@@ -16,64 +16,78 @@ def scrape_data():
     # CBC news data
     URL_CBC = "https://www.cbc.ca/news"
     page = requests.get(URL_CBC)
-    soup = BeautifulSoup(page.content, "lxml")
 
-    flag = validation_data(soup.find_all("h3", class_="headline"), soup.find(
-        "a", class_="primaryHeadlineLink sclt-contentpackageheadline"))
-    if flag:
-        data_dict["CBC"] = {"Title": soup.find_all("h3", class_="headline")[0].string, "URL": "https://www.cbc.ca" +
-                            soup.find("a", class_="primaryHeadlineLink sclt-contentpackageheadline")["href"]}
-    else:
-        data_dict["CBC"] = {"Title": "No title", "URL": "No url"}
+    if page.status_code == 200:
+        soup = BeautifulSoup(page.content, "lxml")
+
+        flag = is_empty_scrap_data(soup.find_all("h3", class_="headline"), soup.find(
+            "a", class_="primaryHeadlineLink sclt-contentpackageheadline"))
+        if flag:
+            data_dict["CBC"] = {"Title": soup.find_all("h3", class_="headline")[0].string, "URL": "https://www.cbc.ca" +
+                                soup.find("a", class_="primaryHeadlineLink sclt-contentpackageheadline")["href"]}
+        else:
+            data_dict["CBC"] = {"Title": "", "URL": ""}
 
     # NBC news data
+
     URL_NBC = "https://www.nbcnews.com/"
     page = requests.get(URL_NBC)
-    soup = BeautifulSoup(page.content, "lxml")
 
-    flag = validation_data(soup.find("a", class_="vilynx_disabled"), soup.find(
-        "a", class_="vilynx_disabled")["href"])
+    if page.status_code == 200:
 
-    if flag:
+        soup = BeautifulSoup(page.content, "lxml")
 
-        data_dict["NBC"] = {"Title": soup.find("a", class_="vilynx_disabled").string, "URL": soup.find(
-            "a", class_="vilynx_disabled")["href"]}
-    else:
-        data_dict["NBC"] = {"Title": "No title", "URL": "No url"}
+        flag = is_empty_scrap_data(soup.find("a", class_="vilynx_disabled"), soup.find(
+            "a", class_="vilynx_disabled")["href"])
+
+        if flag:
+
+            data_dict["NBC"] = {"Title": soup.find("a", class_="vilynx_disabled").string, "URL": soup.find(
+                "a", class_="vilynx_disabled")["href"]}
+        else:
+            data_dict["NBC"] = {"Title": "", "URL": ""}
 
     # YAHOO news data
     URL_YAHOO = "https://ca.news.yahoo.com/"
     page = requests.get(URL_YAHOO)
-    soup = BeautifulSoup(page.content, "lxml")
 
-    flag = validation_data(soup.find_all("h2"), soup.find(
-        id="item-0").find("a", class_="Td(n)"))
+    if page.status_code == 200:
 
-    if flag:
+        soup = BeautifulSoup(page.content, "lxml")
 
-        data_dict["YAHOO"] = {"Title": soup.find_all("h2")[1].string, "URL": "https://ca.news.yahoo.com" +
-                              soup.find(id="item-0").find("a", class_="Td(n)")["href"]}
-    else:
-        data_dict["YAHOO"] = {"Title": "No title", "URL": "No url"}
+        flag = is_empty_scrap_data(soup.find_all("h2"), soup.find(
+            id="item-0").find("a", class_="Td(n)"))
+
+        if flag:
+
+            data_dict["YAHOO"] = {"Title": soup.find_all("h2")[1].string, "URL": "https://ca.news.yahoo.com" +
+                                  soup.find(id="item-0").find("a", class_="Td(n)")["href"]}
+        else:
+            data_dict["YAHOO"] = {"Title": "", "URL": ""}
 
     # GOOGLE news data
     URL_GOOGLE = "https://news.google.com/"
     page = requests.get(URL_GOOGLE)
-    soup = BeautifulSoup(page.content, "lxml")
 
-    flag = validation_data(soup.find("a", class_="DY5T1d"),
-                           soup.find("a", class_="VDXfz"))
-    if flag:
-        data_dict["GOOGLE"] = {"Title": soup.find("a", class_="DY5T1d").string, "URL": "https://news.google.com" +
-                               soup.find("a", class_="VDXfz")["href"].split(".")[1]}
-    else:
-        data_dict["GOOGLE"] = {"Title": "No title", "URL": "No url"}
+    if page.status_code == 200:
+
+        soup = BeautifulSoup(page.content, "lxml")
+
+        flag = is_empty_scrap_data(soup.find("a", class_="DY5T1d"),
+                                   soup.find("a", class_="VDXfz"))
+        if flag:
+            data_dict["GOOGLE"] = {"Title": soup.find("a", class_="DY5T1d").string, "URL": "https://news.google.com" +
+                                   soup.find("a", class_="VDXfz")["href"].split(".")[1]}
+        else:
+            data_dict["GOOGLE"] = {"Title": "", "URL": ""}
+
     print(data_dict)
 
 
-def validation_data(title, url):
+def is_empty_scrap_data(title, url):
     """
-    Validate scrape data
+    check empty scrape data 
+    if empty can't read that data from db
 
     title: str, news title
     url: str, news url
@@ -98,9 +112,6 @@ def add_data_to_db(news_data):
         News.delete_news_data(news_list)
 
     News.register_news_data(news_data)
-
-
-scrape_data()
 
 
 if __name__ == "__main__":
