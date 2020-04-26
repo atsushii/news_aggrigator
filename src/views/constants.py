a65414084b68dae605609215e47eca70b716c09c
Atsushi Miyamoto <atsushiflee@icloud.com>
2020-04-25 20:53:28 -0700

    Delete constants.py

--- src/views/constants.py
+++
@@ -1,5 +0,0 @@
-NEWS_URL = {"CBC": "https://www.cbc.ca/news",
-            "NBC": "https://www.nbcnews.com/",
-            "YAHOO": "https://ca.news.yahoo.com/",
-            "GOOGLE": "https://news.google.com/"
-            }
