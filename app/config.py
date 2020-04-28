5d0a7c1055a6113cb42ef56b045850c5c3d3ba04
atsushiflee <atsushiflee@icloud.com>
2020-04-27 23:29:34 -0700

    Add config

--- app/config.py
+++ app/config.py
@@ -1,1 +1,17 @@
-DEBUG = True
+
+class DevConfig:
+    # Flask
+    DEBUG = True
+
+    # SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@{}/{}'.format(
+        "root",
+        "root",
+        "mysql",
+        "news"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
+
+
+Config = DevConfig
