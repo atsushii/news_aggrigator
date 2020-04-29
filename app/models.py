59b88e3044759662d6b638c4052d8dc8dd32e4d0
atsushiflee <atsushiflee@icloud.com>
2020-04-28 20:47:10 -0700

    model

--- app/models.py
+++ app/models.py
@@ -12,34 +12,7 @@ class News(db.Model):
     _id = db.Column("_id", db.Integer, primary_key=True)
     news_title = db.Column("news_title", db.String(10))
     news_head_line = db.Column("news_head_line", db.String(100))
-    news_url = db.column("news_url", db.Text)
+    news_url = db.Column("news_url", db.Text)
 
     def __repr__(self):
         return f"News('{self.news_title}', '{self.news_head_line}', '{self.news_url}')"
-
-    def get_news_data(self):
-        """
-        null check
-
-        return: bool
-        """
-
-        news_data = db.session.query(News).all()
-
-        if news_data:
-            return news_data
-        return ""
-
-    def delete_news_data(self, data):
-        """
-        delete if data is in db
-        """
-        db.session.delete(data)
-        db.session.commit()
-
-    def register_news_data(self, data):
-        """
-        Add latest news data to db
-
-        data: dict, dictionary of news data
-        """
