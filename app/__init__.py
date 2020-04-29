ad8fe75be5e4c970288254955e5d532f97b15b6f
atsushiflee <atsushiflee@icloud.com>
2020-04-28 20:46:24 -0700

    Add batch

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,48 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from apscheduler.schedulers.background import BackgroundScheduler
 
 db = SQLAlchemy()
 
 
+def do():
+    scrape = Scrape()
+    data = scrape.scrape_news()
+    execute_db = Execute_db()
+    execute_db.delete()
+    execute_db.post(data)
+    print("complete")
+
+
+def start_batch():
+    scheduler = BackgroundScheduler(demon=True)
+    scheduler.add_job(do, 'interval', minutes=1)
+    scheduler.start()
+    print("start")
+
+
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile("config.py")
+    app.config.from_object('app.config.Config')
 
     from .views.views import view_blueprint
     app.register_blueprint(view_blueprint)
 
     from app.models import db
-    db.init_app(app)
+    from app.utils.execute_db import db
 
-    return app
+    db.init_app(app)
+    Migrate(app, db)
+    from app.utils.scrape import Scrape
+    from app.utils.execute_db import Execute_db
+
+    return app, Execute_db, Scrape
+
+
+app, Execute_db, Scrape = create_app()
+
+app.app_context().push()
+
+
+start_batch()
